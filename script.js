777aa091edeac9e4a936bf7c2cc052401d759cf4
Natalie Gillam <ngillam@hotmail.co.uk>
2024-10-24 21:34:30 +0100

    started function to convert numbers to words

--- script.js
+++ script.js
@@ -29,3 +29,68 @@ function calculate() {
   }
 }
 
+
+const ones = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine", }
+
+const tenToNineteen = {
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+}
+
+const tensColumn = {
+    2: "twenty",
+    3: "thirty",
+    4: "forty",
+    5: "fifty",
+    6: "sixty",
+    7: "seventy",
+    8: "eighty",
+    9: "ninety",
+}
+
+const suffixes = {
+    1: "",
+    2: "thousand",
+    3: "million",
+    4: "billion",
+    5: "trillion",
+}
+
+function numberToWords(){
+    calculate();
+    const number = result.innerText;
+
+    const limit = 1000000000000
+
+    const numberInWords = "";
+
+    
+
+//     if (number in underTwenty){
+//         result.innerText = underTwenty[number];
+//     }
+   
+//     const arrayOfDigits = result.innerText.split("")
+//     if(arrayOfDigits.length ==2 && number > 19 ){
+// result.innerText = `${tensColumn[arrayOfDigits[0]]} ${underTwenty[arrayOfDigits[1]]}`
+//     }
+}
+
