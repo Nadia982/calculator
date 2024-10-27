6164b3c3fdf2a84f0f0b13070b4c18172e11dae7
Natalie Gillam <ngillam@hotmail.co.uk>
2024-10-27 22:34:09 +0000

    added ability to conver to Roman numerals; updated some styles

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function clearDisplay() {
   result.innerText = "";
 }
 
-function toggleSign(){
-        let answer = eval(result.innerText * -1);
-        result.innerText = answer;
+function toggleSign() {
+  let answer = eval(result.innerText * -1);
+  result.innerText = answer;
 }
 
-function backspace(){
-    calculation.innerText = calculation.innerText.slice(0, -1);
-    result.innerText = calculation.innerText;
+function backspace() {
+  calculation.innerText = calculation.innerText.slice(0, -1);
+  result.innerText = calculation.innerText;
 }
 
 function calculate() {
@@ -29,8 +29,56 @@ function calculate() {
   }
 }
 
-function numberToWords(){
-const ones = {
+function numberToRomanNumerals() {
+
+  calculate(); //calculate the answer for the result box in case it hasn't already been calculated.
+  let num = result.innerText;
+  if(num>3999999){
+    result.innerText ="Error: limit for Roman numeral conversion is 3,999,999"
+  } else {
+  const romanNumerals = {
+    M̅: 1000000,
+    C̅M̅: 900000,
+    D̄: 500000,
+    C̅D̄: 400000,
+    C̅: 100000,
+    X̅C̅: 90000,
+    L̅: 50000,
+    X̅L̅: 40000,
+    X̅: 10000,
+    V̅: 5000,
+    MV̅: 4000,
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+   L: 50,
+   XL: 40, 
+   X: 10,
+   IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let output = "";
+  for(const letter in romanNumerals){
+    const numberEquivalentToRomanNumeral = romanNumerals[letter]; 
+    while(num >= numberEquivalentToRomanNumeral){
+      num -= numberEquivalentToRomanNumeral;
+      output += letter;
+    }
+  }
+
+result.innerText = output;
+}}
+
+function numberToWords() {
+  calculate(); //calculate the answer for the result box in case it hasn't already been calculated.
+
+  const ones = {
     0: "zero",
     1: "one",
     2: "two",
@@ -40,9 +88,10 @@ const ones = {
     6: "six",
     7: "seven",
     8: "eight",
-    9: "nine", }
+    9: "nine",
+  };
 
-const tenToNineteen = {
+  const tenToNineteen = {
     10: "ten",
     11: "eleven",
     12: "twelve",
@@ -53,9 +102,9 @@ const tenToNineteen = {
     17: "seventeen",
     18: "eighteen",
     19: "nineteen",
-}
+  };
 
-const tensColumn = {
+  const tensColumn = {
     2: "twenty",
     3: "thirty",
     4: "forty",
@@ -64,41 +113,36 @@ const tensColumn = {
     7: "seventy",
     8: "eighty",
     9: "ninety",
-}
+  };
 
-const suffixes = {
+  const suffixes = {
     1: "",
     2: "thousand",
     3: "million",
     4: "billion",
     5: "trillion",
-}
+  };
 
-    // Obtaining input to convert number to words
-    calculate(); //calculate the answer for the result box in case it hasn't already been calculated.
-    const numText = "";
-    const num = result.innerText;
-    const finalNumText = convertNum(num);
-    const limit = 1000000000000;
-    result.innerText = finalNumText;
+  // Obtaining input to convert number to words
+  const numText = "";
+  const num = result.innerText;
+  const finalNumText = convertNum(num);
+  const limit = 1000000000000;
+  result.innerText = finalNumText;
 
-
-    function convertNum(num) {
-        let numStr = num.toString().split(".");
-        let wholeNum = numStr[0];
-        let decimalNum = numStr[1];
-        let convertedWholeNum = "";
-
-}
-
+  function convertNum(num) {
+    let numStr = num.toString().split(".");
+    let wholeNum = numStr[0];
+    let decimalNum = numStr[1];
+    let convertedWholeNum = "";
+  }
 }
 
 //     if (number in underTwenty){
 //         result.innerText = underTwenty[number];
 //     }
-   
+
 //     const arrayOfDigits = result.innerText.split("")
 //     if(arrayOfDigits.length ==2 && number > 19 ){
 // result.innerText = `${tensColumn[arrayOfDigits[0]]} ${underTwenty[arrayOfDigits[1]]}`
 //     }
-
