cdcb95c937ecac2ecf9f671510492ebaafea3b32
Natalie Gillam <ngillam@hotmail.co.uk>
2024-10-28 00:47:12 +0000

    set limits on size of input and calculation results

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const calculation = document.getElementById("calculation");
 const result = document.getElementById("result");
 
 function appendToDisplay(input) {
+  if(calculation.innerText.length<25){
   calculation.innerText += input;
+  }else {
+    result.style.fontSize = "32px";
+    result.innerText = "Error: this calculator supports an input of up to 25 digits";
+  }
+   
 }
 
 function clearDisplay() {
@@ -21,64 +27,74 @@ function backspace() {
 }
 
 function calculate() {
+  result.style.fontSize = "62px";
   try {
     let answer = eval(calculation.innerText);
+    if(answer<1000000000000){
     result.innerText = parseFloat(answer.toFixed(10));
+    } 
+    else {
+      result.style.fontSize = "28px";
+      result.innerText = "This calculator only shows results up to 1,000,000,000,000";
+    }
   } catch (error) {
     result.innerText = "Error";
   }
 }
 
 function numberToRomanNumerals() {
-
   calculate(); //calculate the answer for the result box in case it hasn't already been calculated.
   let num = result.innerText;
-  if(num>3999999){
-    result.innerText ="Error: limit for Roman numeral conversion is 3,999,999"
+  if (num > 3999999) {
+    result.style.fontSize = "32px";
+    result.innerText =
+      "Error: max value to convert to Roman numerals is 3,999,999";
   } else {
-  const romanNumerals = {
-    M̅: 1000000,
-    C̅M̅: 900000,
-    D̄: 500000,
-    C̅D̄: 400000,
-    C̅: 100000,
-    X̅C̅: 90000,
-    L̅: 50000,
-    X̅L̅: 40000,
-    X̅: 10000,
-    V̅: 5000,
-    MV̅: 4000,
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-   L: 50,
-   XL: 40, 
-   X: 10,
-   IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
+    const romanNumerals = {
+      M̅: 1000000,
+      C̅M̅: 900000,
+      D̄: 500000,
+      C̅D̄: 400000,
+      C̅: 100000,
+      X̅C̅: 90000,
+      L̅: 50000,
+      X̅L̅: 40000,
+      X̅: 10000,
+      V̅: 5000,
+      MV̅: 4000,
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    };
 
-  let output = "";
-  for(const letter in romanNumerals){
-    const numberEquivalentToRomanNumeral = romanNumerals[letter]; 
-    while(num >= numberEquivalentToRomanNumeral){
-      num -= numberEquivalentToRomanNumeral;
-      output += letter;
+    let romanNumeralOutput = "";
+    for (const letter in romanNumerals) {
+      const numberEquivalentToRomanNumeral = romanNumerals[letter];
+      while (num >= numberEquivalentToRomanNumeral) {
+        num -= numberEquivalentToRomanNumeral;
+        romanNumeralOutput += letter;
+      }
     }
-  }
 
-result.innerText = output;
-}}
+    result.innerText = romanNumeralOutput;
+  }
+}
 
 function numberToWords() {
   calculate(); //calculate the answer for the result box in case it hasn't already been calculated.
-
-  const ones = {
+  let number = result.innerText;
+   
+    const zeroToNine = {
     0: "zero",
     1: "one",
     2: "two",
@@ -124,25 +140,33 @@ function numberToWords() {
   };
 
   // Obtaining input to convert number to words
-  const numText = "";
+  let wordOutput = "";
   const num = result.innerText;
   const finalNumText = convertNum(num);
-  const limit = 1000000000000;
-  result.innerText = finalNumText;
+  const numLimit = 1000000000000;
+  
 
   function convertNum(num) {
-    let numStr = num.toString().split(".");
-    let wholeNum = numStr[0];
-    let decimalNum = numStr[1];
-    let convertedWholeNum = "";
+    const absNum = Math.abs(num);
+
+    try{
+      if(num> numLimit){
+        result.style.fontSize = "32px";
+        throw "Error: max value to convert to words is 1,000,000,000";
+      }
+    }
+    catch(err){
+      // alert(err)
+      // console.log("Error: max value to convert to words is 1,000,000,000")
+      result.style.fontSize = "32px";
+      result.innerText = "Error: max value to convert to words is 1,000,000,000";
+      return;
+    }
+
+    // let numStr = num.toString().split(".");
+    // let wholeNum = numStr[0];
+    // let decimalNum = numStr[1];
+    // let convertedWholeNum = "";
   }
+  // result.innerText = finalNumText;
 }
-
-//     if (number in underTwenty){
-//         result.innerText = underTwenty[number];
-//     }
-
-//     const arrayOfDigits = result.innerText.split("")
-//     if(arrayOfDigits.length ==2 && number > 19 ){
-// result.innerText = `${tensColumn[arrayOfDigits[0]]} ${underTwenty[arrayOfDigits[1]]}`
-//     }
