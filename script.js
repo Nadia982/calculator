730520e81f1bee6d5bdbf0b3d754dcffac67da69
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-16 01:24:33 +0000

    refined 'isDigit' function

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 console.clear();
-// Query selectors 
+// Query selectors
 const display = document.getElementById("display");
 const result = document.getElementById("result");
 result.innerText = "0";
 result.style.fontSize = "56px";
-
 
 function appendToDisplay(input) {
   let maxInputLength = 30;
@@ -36,7 +35,7 @@ function processOperation(values, operators) {
   if (operator === "*") values.push(left * right);
   if (operator === "/") values.push(left / right);
 }
-// evaluate expression
+
 function evaluateExpression(expression) {
   let values = [];
   let operators = [];
@@ -45,34 +44,44 @@ function evaluateExpression(expression) {
   while (i < expression.length) {
     let char = expression[i];
 
-    if (isDigit(char) || char === '.') {
+    if (isDigitOrDecimalPlace(char)) {
       let number = 0;
       let decimalPlace = 1;
       let hasDecimal = false;
 
       // Parse the entire number, including any decimals
-      while (i < expression.length && (isDigit(expression[i]) || expression[i] === '.')) {
-        if (expression[i] === '.') {
+      while (
+        i < expression.length &&
+        (isDigitOrDecimalPlace(expression[i]))
+      ) {
+        if (expression[i] === ".") {
           hasDecimal = true;
         } else {
           if (hasDecimal) {
             decimalPlace /= 10;
-            number += (expression[i] - '0') * decimalPlace;
+            number += (expression[i] - "0") * decimalPlace;
           } else {
-            number = number * 10 + (expression[i] - '0');
+            number = number * 10 + (expression[i] - "0");
           }
         }
         i++;
       }
       values.push(number);
       i--;
-    } else if (char === '+' || char === '-' || char === '*' || char === '/') {
-      if (char === '-' && i + 1 < expression.length && expression[i + 1] === '-') {
-        i++; 
-        char = '+';
+    } else if (char === "+" || char === "-" || char === "*" || char === "/") {
+      if (
+        char === "-" &&
+        i + 1 < expression.length &&
+        expression[i + 1] === "-"
+      ) {
+        i++;
+        char = "+";
       }
 
-      while (operators.length && precedence(operators[operators.length - 1]) >= precedence(char)) {
+      while (
+        operators.length &&
+        precedence(operators[operators.length - 1]) >= precedence(char)
+      ) {
         processOperation(values, operators);
       }
       operators.push(char);
@@ -87,8 +96,8 @@ function evaluateExpression(expression) {
   return values.pop();
 }
 
-function isDigit(char) {
-  return char >= '0' && char <= '9';
+function isDigitOrDecimalPlace(char) {
+  return char.match(/[0-9.]/);
 }
 
 function precedence(op) {
@@ -99,21 +108,23 @@ function precedence(op) {
 
 function calculate() {
   result.style.fontSize = "56px";
-  if(display.innerText.length === 0){
-        result.innerText = 0;
+  if (display.innerText.length === 0) {
+    result.innerText = 0;
+  } else {
+    try {
+      let answer = evaluateExpression(display.innerText);
+      if (answer < 1000000000) {
+        result.innerText = parseFloat(answer.toFixed(6));
       } else {
-  try {
-    let answer = evaluateExpression(display.innerText);
-    if (answer < 1000000000) {
-      result.innerText = parseFloat(answer.toFixed(6));
-    } else {
-      result.style.fontSize = "28px";
-      result.innerText = "This calculator only shows results up to 10 digits long";
+        result.style.fontSize = "28px";
+        result.innerText =
+          "This calculator only shows results up to 10 digits long";
+      }
+    } catch (error) {
+      result.innerText = "Error";
     }
-  } catch (error) {
-    result.innerText = "Error";
   }
-}}
+}
 
 function numberToRomanNumerals() {
   calculate();
@@ -164,146 +175,149 @@ function numberToRomanNumerals() {
 }
 
 function convertNumberToWords() {
-    calculate(); //calculates result if this hasn't already been done
-    const num = result.innerText;
-    const maxNumLimit = 1000000; 
-    const minNumLimit = -maxNumLimit;
-    result.style.fontSize = "32px";
+  calculate(); //calculates result if this hasn't already been done
+  const num = result.innerText;
+  const maxNumLimit = 1000000;
+  const minNumLimit = -maxNumLimit;
+  result.style.fontSize = "32px";
 
-    if (num > maxNumLimit) {
-            result.innerText = "Error: max value to convert to words is 1,000,000"; 
-      return;
-    }
-    if (num < minNumLimit) {
-      result.innerText = "Error: min value to convert to words is -1,000,000";
-      return;
-    }
+  if (num > maxNumLimit) {
+    result.innerText = "Error: max value to convert to words is 1,000,000";
+    return;
+  }
+  if (num < minNumLimit) {
+    result.innerText = "Error: min value to convert to words is -1,000,000";
+    return;
+  }
 
-    result.innerText = numberToWords(num);
+  result.innerText = numberToWords(num);
+}
+
+function numberToWords(num) {
+  if (num == 0) {
+    return "zero";
+  } else if (num < 0) {
+    const positiveNum = num.toString().slice(1);
+    return "minus " + numberToWords(positiveNum);
+  } else if (num.includes(".")) {
+    const [integerPart, decimalPart] = num.split(".");
+    return (
+      convertPositiveNumberToWords(parseInt(integerPart)) +
+      " point " +
+      convertDecimalPartToWords(decimalPart)
+    );
+  } else {
+    return convertPositiveNumberToWords(num);
   }
-  
-  function numberToWords(num) {
-    if (num == 0) {
-      return "zero";
-    } else if (num < 0) {
-      const positiveNum = num.toString().slice(1);
-      return "minus " + numberToWords(positiveNum);
-    } else if (num.includes('.')) {
-      const [integerPart, decimalPart] = num.split('.');
-      return convertPositiveNumberToWords(parseInt(integerPart)) + " point " + convertDecimalPartToWords(decimalPart);
-    } else {
-      return convertPositiveNumberToWords(num);
-    }
+}
+
+function convertDecimalPartToWords(decimalStr) {
+  const digits = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+  };
+
+  let decimalWords = "";
+  for (let digit of decimalStr) {
+    decimalWords += digits[digit] + " ";
   }
-  
-  function convertDecimalPartToWords(decimalStr) {
-    const digits = {
-      0: "zero",
-      1: "one",
-      2: "two",
-      3: "three",
-      4: "four",
-      5: "five",
-      6: "six",
-      7: "seven",
-      8: "eight",
-      9: "nine"
-    };
-    
-    let decimalWords = "";
-    for (let digit of decimalStr) {
-      decimalWords += digits[digit] + " ";
-    }
-    return decimalWords.trim();
-  }
-  
-  function convertPositiveNumberToWords(num) {
-    const ones = {
-      1: "one",
-      2: "two",
-      3: "three",
-      4: "four",
-      5: "five",
-      6: "six",
-      7: "seven",
-      8: "eight",
-      9: "nine",
-    };
-    const teens = {
-      10: "ten",
-      11: "eleven",
-      12: "twelve",
-      13: "thirteen",
-      14: "fourteen",
-      15: "fifteen",
-      16: "sixteen",
-      17: "seventeen",
-      18: "eighteen",
-      19: "nineteen",
-    };
-    const tens = {
-      2: "twenty",
-      3: "thirty",
-      4: "forty",
-      5: "fifty",
-      6: "sixty",
-      7: "seventy",
-      8: "eighty",
-      9: "ninety",
-    };
-  
-    let words = "";
-  
-    function handleOneToNinetyNine(num) {
-      if (num > 0 && num < 10) {
-        words += ones[num];
-      } else if (num >= 10 && num < 20) {
-        words += teens[num];
-      } else if (num >= 20 && num < 100) {
-        words += tens[Math.floor(num / 10)];
-        if (num % 10 > 0) {
-          words += " " + ones[num % 10];
-        }
+  return decimalWords.trim();
+}
+
+function convertPositiveNumberToWords(num) {
+  const ones = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+  };
+  const teens = {
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+  };
+  const tens = {
+    2: "twenty",
+    3: "thirty",
+    4: "forty",
+    5: "fifty",
+    6: "sixty",
+    7: "seventy",
+    8: "eighty",
+    9: "ninety",
+  };
+
+  let words = "";
+
+  function handleOneToNinetyNine(num) {
+    if (num > 0 && num < 10) {
+      words += ones[num];
+    } else if (num >= 10 && num < 20) {
+      words += teens[num];
+    } else if (num >= 20 && num < 100) {
+      words += tens[Math.floor(num / 10)];
+      if (num % 10 > 0) {
+        words += " " + ones[num % 10];
       }
-      return words;
     }
-  
-    function handle100to999(num) {
-      words += ones[Math.floor(num / 100)] + " hundred";
-      if (num % 100 > 0) {
-        words += " and ";
-        handleOneToNinetyNine(num % 100);
-      }
-      return words;
-    }
-  
-    function handle1000to999999(num) {
-      const thousands = Math.floor(num / 1000);
-      if (thousands < 100) {
-        handleOneToNinetyNine(thousands);
-      } else {
-        handle100to999(thousands);
-      }
-      words += " thousand";
-      if (num % 1000 > 0) {
-        if (num % 1000 < 100) {
-          words += " and ";
-        } else {
-          words += ", ";
-        }
-        handle100to999(num % 1000);
-      }
-      return words;
-    }
-  
-    if (num < 100) {
-      handleOneToNinetyNine(num);
-    } else if (num >= 100 && num < 1000) {
-      handle100to999(num);
-    } else if (num >= 1000 && num < 1000000) {
-      handle1000to999999(num);
-    }
-  
     return words;
   }
-  
+
+  function handle100to999(num) {
+    words += ones[Math.floor(num / 100)] + " hundred";
+    if (num % 100 > 0) {
+      words += " and ";
+      handleOneToNinetyNine(num % 100);
+    }
+    return words;
+  }
+
+  function handle1000to999999(num) {
+    const thousands = Math.floor(num / 1000);
+    if (thousands < 100) {
+      handleOneToNinetyNine(thousands);
+    } else {
+      handle100to999(thousands);
+    }
+    words += " thousand";
+    if (num % 1000 > 0) {
+      if (num % 1000 < 100) {
+        words += " and ";
+      } else {
+        words += ", ";
+      }
+      handle100to999(num % 1000);
+    }
+    return words;
+  }
+
+  if (num < 100) {
+    handleOneToNinetyNine(num);
+  } else if (num >= 100 && num < 1000) {
+    handle100to999(num);
+  } else if (num >= 1000 && num < 1000000) {
+    handle1000to999999(num);
+  }
+
+  return words;
+}
